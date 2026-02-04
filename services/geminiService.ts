8eb511a65b26ff6a4421c5b74505106f528a1c53
tramanpham161 <thehonestmumreviews@gmail.com>
2026-02-04 21:51:00 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,6 +1,6 @@
 
 import { GoogleGenAI, Type } from "@google/genai";
-import { ParentData } from "../types";
+import { ParentData } from "../types.ts";
 
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
